00e8b080444f32f110b0c72493bd7036b7234e07
MdRaheel <m.raheelxda@gmail.com>
2016-12-30 17:57:19 +0530

    Test support FN

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@
 export ARCH=arm
 export CROSS_COMPILE=/home/raheel/Desktop/Toolchain/Sabermod4.8/bin/arm-eabi-
 mkdir output
-
+#make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5nlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig#
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j53g_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j64 -C $(pwd) O=output
 
